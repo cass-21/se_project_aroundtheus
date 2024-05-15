7b6611e4c2b1f1411a9d8dd3341032bf03b9c553
cass-21 <160291710+cass-21@users.noreply.github.com>
2024-05-14 21:38:55 -0400

    JavaScript Importing

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const initialCards = [
+    {
+      name: "Grand Canyon, AZ",
+      link: "https://unsplash.com/photos/landscape-of-mountain-HleqO6kuL9c"
+    },
+    {
+      name: "Key West, FL",
+      link: "https://unsplash.com/photos/gray-wooden-dock-on-body-of-water-during-daytime-LwPmzW65U4M"
+    },
+    {
+      name: "Napali Coast, HI",
+      link: "https://unsplash.com/photos/birds-eye-view-photography-of-mountain-wJl599nQyNA"
+    },
+    {
+      name: "New York City, NY",
+      link: "https://unsplash.com/photos/time-square-new-york-during-daytime-TaCk3NspYe0"
+    },
+    {
+      name: "Portage, AK",
+      link: "https://unsplash.com/photos/northern-lights-3OR-XFzKSBo"
+    },
+    {
+      name: "Seattle, WA",
+      link: "https://unsplash.com/photos/aerial-view-of-buildings-RpvC77-exG0"
+    }
+  ];
+  
