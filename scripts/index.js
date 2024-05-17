ce980bf5a0b907b212f65223d3f74b7cd0ab279c
cass-21 <160291710+cass-21@users.noreply.github.com>
2024-05-17 15:04:52 -0400

    Cards Array in JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,63 @@
+const cardData1 = {
+  name: "Grand Canyon, AZ",
+  link: "https://unsplash.com/photos/landscape-of-mountain-HleqO6kuL9c"
+  };
+
+  const cardData2 = {
+    name: "Key West, FL",
+    link: "https://unsplash.com/photos/gray-wooden-dock-on-body-of-water-during-daytime-LwPmzW65U4M"
+  };
+
+  const cardData3 = {
+    name: "Napali Coast, HI",
+    link: "https://unsplash.com/photos/birds-eye-view-photography-of-mountain-wJl599nQyNA"
+  };
+
+  const cardData4 = { 
+    name: "New York City, NY",
+    link: "https://unsplash.com/photos/time-square-new-york-during-daytime-TaCk3NspYe0"
+  };
+
+  const cardData5 = {
+    name: "Portage, AK",
+    link: "https://unsplash.com/photos/northern-lights-3OR-XFzKSBo"
+  };
+
+  const cardData6 = {
+    name: "Seattle, WA",
+    link: "https://unsplash.com/photos/aerial-view-of-buildings-RpvC77-exG0"
+  };
+
 const initialCards = [
-    {
-      name: "Grand Canyon, AZ",
-      link: "https://unsplash.com/photos/landscape-of-mountain-HleqO6kuL9c"
-    },
-    {
-      name: "Key West, FL",
-      link: "https://unsplash.com/photos/gray-wooden-dock-on-body-of-water-during-daytime-LwPmzW65U4M"
-    },
-    {
-      name: "Napali Coast, HI",
-      link: "https://unsplash.com/photos/birds-eye-view-photography-of-mountain-wJl599nQyNA"
-    },
-    {
-      name: "New York City, NY",
-      link: "https://unsplash.com/photos/time-square-new-york-during-daytime-TaCk3NspYe0"
-    },
-    {
-      name: "Portage, AK",
-      link: "https://unsplash.com/photos/northern-lights-3OR-XFzKSBo"
-    },
-    {
-      name: "Seattle, WA",
-      link: "https://unsplash.com/photos/aerial-view-of-buildings-RpvC77-exG0"
-    }
-  ];
-  
+  {
+    name: "Grand Canyon, AZ",
+    link: "https://unsplash.com/photos/landscape-of-mountain-HleqO6kuL9c"
+  },
+
+  {
+    name: "Key West, FL",
+    link: "https://unsplash.com/photos-gray-wooden-dock-on-body-of-water-during-daytime-LwPmzW65U4M"
+  },
+
+  {
+    name: "Napali Coast, HI",
+    link: "https://unsplash.com/photos/birds-eye-view-photography-of-mountain-wJl599nQyNA"
+  },
+
+  {
+    name: "New York City, NY",
+    link: "https://unsplash.com/photos/time-square-new-york-during-daytime-TaCk3NspYe0"
+  },
+
+  {
+    name: "Portage, AK",
+    link: "https://unsplash.com/photos/northern-lights-3OR-XFzKSBo"
+  },
+
+  {
+    name: "Seattle, WA",
+link: "https://unsplash.com/photos/aerial-view-of-buildings-RpvC77-exG0"
+  },
+];
+
+console.log(initialCards);
