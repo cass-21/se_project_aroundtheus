d6fe7846230824b2a9b95bd09b13b1615c7ec951
cass-21 <160291710+cass-21@users.noreply.github.com>
2024-05-23 07:42:39 -0400

    Modal opens and closes

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,30 @@ const initialCards = [
   }
 ];
 
-console.log(initialCards);
+/* ----------------------------------------------------------------------------------------------------- */
+/*                                                  Elements                                                          */
+/* ----------------------------------------------------------------------------------------------------- */
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector("#edit-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description"); 
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+
+
+
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent; 
+ 
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseButton.addEventListener("click", () => {
+
+  profileEditModal.classList.remove("modal_opened");
+});
+  
